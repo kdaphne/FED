5b8d1df14dbc03903c5a30cc3f2b244ee01a8247
kdaphne <112805898+kdaphne@users.noreply.github.com>
2022-09-21 19:30:45 +0200

    Add files via upload

--- Js/vanharen.js
+++ Js/vanharen.js
@@ -3,6 +3,6 @@ var menuButton= document.querySelector("header nav:nth-of-type(2) button");
 menuButton.addEventListener("click", menuOpenen);
 
 function menuOpenen () {
-    var deNav= document.querySelector("header nav:nth-of-type(2)");
-    deNav.classList.toggle(menuGaatOpenenDicht);
+    var deNav = document.querySelector("header nav:nth-of-type(2)");
+    deNav.classList.toggle("menuGaatOpenEnDicht");
 }
