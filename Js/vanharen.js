1bbc4eabeaa37fcc7ba4d728aee14fe9adbdb80a
kdaphne <112805898+kdaphne@users.noreply.github.com>
2022-09-27 17:22:24 +0200

    Add files via upload

--- Js/vanharen.js
+++ Js/vanharen.js
@@ -6,3 +6,14 @@ function menuOpenen () {
     var deNav = document.querySelector("header nav:nth-of-type(2)");
     deNav.classList.toggle("menuGaatOpenEnDicht");
 }
+
+
+
+// var favorietButton= document.querySelector("main section:nth-of-type(3) li button");
+
+// favorietButton.addEventListener("click", hartjeVullen);
+
+// function hartjeVullen () {
+//     var hetHartje = document.querySelector("main section:nth-of-type(3) button");
+//     hetHartje.classList.toggle("favorietMaken");
+// }
