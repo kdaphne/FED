145fe6a08a1b0fec9fd6f6e3d60e96da5c364a98
kdaphne <112805898+kdaphne@users.noreply.github.com>
2022-10-04 17:36:45 +0200

    Add files via upload

--- Js/vanharen.js
+++ Js/vanharen.js
@@ -7,6 +7,32 @@ function menuOpenen () {
     deNav.classList.toggle("menuGaatOpenEnDicht");
 }
 
+var kiesMaat= document.querySelector("main section:nth-of-type(4) button");
+
+kiesMaat.addEventListener("click", lijstMetMaten);
+
+
+function lijstMetMaten () {
+    alert("hoihoihoi")
+    var alleMaten = document.querySelector("main section:nth-of-type(4) ul");
+    alleMaten.classList.add("matenKomen");
+}
+
+
+
+
+// bron Russel, snap ik niet
+var maatHouden= document.querySelector("main section:nth-of-type(4) ul button");
+
+maatHouden.addEventListener("click", schoenPagina);
+
+
+function schoenPagina () {
+    console.log("nu moet het weggaan")
+    var overzichtSchoen = document.querySelector("main section:nth-of-type(4) ul");
+    overzichtSchoen.classList.remove("matenKomen");
+}
+
 
 
 // var favorietButton= document.querySelector("main section:nth-of-type(3) li button");
--- Js/vanharendetail.js
+++ Js/vanharendetail.js
@@ -1,25 +1,25 @@
-var kiesMaat= document.querySelector("main section:nth-of-type(4) button");
+// var kiesMaat= document.querySelector("main section:nth-of-type(4) button");
 
-kiesMaat.addEventListener("click", lijstMetMaten);
-
-
-function lijstMetMaten () {
-    alert("hoihoihoi")
-    var alleMaten = document.querySelector("main section:nth-of-type(4) ul");
-    alleMaten.classList.add("matenKomen");
-}
+// kiesMaat.addEventListener("click", lijstMetMaten);
 
 
+// function lijstMetMaten () {
+//     alert("hoihoihoi")
+//     var alleMaten = document.querySelector("main section:nth-of-type(4) ul");
+//     alleMaten.classList.add("matenKomen");
+// }
 
 
-// bron Russel, snap ik niet
-var maatHouden= document.querySelector("main section:nth-of-type(4) ul button");
-
-maatHouden.addEventListener("click", schoenPagina);
 
 
-function schoenPagina () {
-    console.log("nu moet het weggaan")
-    var overzichtSchoen = document.querySelector("main section:nth-of-type(4) ul");
-    overzichtSchoen.classList.remove("matenKomen");
-}
+// // bron Russel, snap ik niet
+// var maatHouden= document.querySelector("main section:nth-of-type(4) ul button");
+
+// maatHouden.addEventListener("click", schoenPagina);
+
+
+// function schoenPagina () {
+//     console.log("nu moet het weggaan")
+//     var overzichtSchoen = document.querySelector("main section:nth-of-type(4) ul");
+//     overzichtSchoen.classList.remove("matenKomen");
+// }
